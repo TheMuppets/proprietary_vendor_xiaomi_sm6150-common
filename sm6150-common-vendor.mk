bc10f42f054ee3d79b60c36b6d2a595c55729ae6
basamaryan <basam.aryan@gmail.com>
2023-10-29 22:08:57 +0100

    sm6150-common: Add missing ADSP modules
    Change-Id: Ic93a70ebf3426de0f3cc355de0d46b766cdca3a7

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -81,6 +81,8 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sm6150-common/proprietary/vendor/lib/rfsa/adsp/misound_karaoke_res.bin:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/misound_karaoke_res.bin \
     vendor/xiaomi/sm6150-common/proprietary/vendor/lib/rfsa/adsp/misound_karaokemix_res.bin:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/misound_karaokemix_res.bin \
     vendor/xiaomi/sm6150-common/proprietary/vendor/lib/rfsa/adsp/misound_res.bin:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/misound_res.bin \
+    vendor/xiaomi/sm6150-common/proprietary/vendor/lib/rfsa/adsp/misound_res_headphone.bin:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/misound_res_headphone.bin \
+    vendor/xiaomi/sm6150-common/proprietary/vendor/lib/rfsa/adsp/misound_res_spk.bin:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/misound_res_spk.bin \
     vendor/xiaomi/sm6150-common/proprietary/vendor/radio/qcril_database/qcril.db:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/qcril.db \
     vendor/xiaomi/sm6150-common/proprietary/vendor/radio/qcril_database/upgrade/0_initial.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/0_initial.sql \
     vendor/xiaomi/sm6150-common/proprietary/vendor/radio/qcril_database/upgrade/10_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/10_version_update_ecc_table.sql \
@@ -162,10 +164,13 @@ PRODUCT_PACKAGES += \
     capi_v2_aptX_CLHDAD_Encoder \
     capi_v2_aptX_Classic \
     capi_v2_aptX_HD \
+    capi_v2_dap_cpdp \
     libVC1Dec \
     libVC1DecDsp_skel \
     libapps_mem_heap \
+    libarc_hta_driver_skel \
     libarcsoft_dualcam_refocus_skel \
+    libarcsoft_hdrplus_hvx_skel \
     libcamera_nn_skel \
     libcvpdsp_skel \
     libdirac-capiv2 \
@@ -177,8 +182,12 @@ PRODUCT_PACKAGES += \
     libfastcvdsp_skel \
     libhexagon_nn_skel \
     libhexagon_nn_skel_secondary \
+    libhta_dsp_skel \
+    libmialgo_rfs_cdsp_skel \
     libscveObjectSegmentation_skel \
     libscveT2T_skel \
+    libsnpe_dsp_v65_domains_v2_skel \
+    libsnpe_dsp_v66_domains_v2_skel \
     libsns_device_mode_skel \
     libsns_low_lat_stream_skel \
     libasphere \
