7a85dbadd23e4f305cfb7e1b53be1f6decf888e9
basamaryan <basam.aryan@gmail.com>
2024-06-23 23:46:33 +0200

    sm6150-common: Add EGL libs symlinks
    Change-Id: Ia23b4da799862b71f7f6e7ca251c88ec910e6569

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -431,4 +431,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libimscamera_jni_libimscamera_jni_symlink64 \
-    libimsmedia_jni_libimsmedia_jni_symlink64
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
